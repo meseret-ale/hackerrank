01c7b9c99f49016185ffdc04c4a3f5898000a763
Meseret Alemnew <84620197+meseret-ale@users.noreply.github.com>
2022-12-01 08:52:39 -0800

    Create climbingLeaderboard.py

---
+++ climbingLeaderboard.py
@@ -0,0 +1,57 @@
+"""
+answer
+An arcade game player wants to climb to the top of the leaderboard and track their ranking. The game uses
+Dense Ranking, so its leaderboard works like this:
+
+The player with the highest score is ranked number  on the leaderboard.
+Players who have equal scores receive the same ranking number, and the next player(s) receive the immediately 
+following ranking number.
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'climbingLeaderboard' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY ranked
+#  2. INTEGER_ARRAY player
+#
+
+def climbingLeaderboard(ranked, player):
+    # Write your code here
+    scores = list(set(ranked))
+    scores.sort()
+    n = len(scores)
+    i = 0
+    result = []
+    for alscore in player:
+        while i < n and scores[i] <= alscore: 
+            i+=1
+        result.append(n-i+1)
+    return result
+    
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ranked_count = int(input().strip())
+
+    ranked = list(map(int, input().rstrip().split()))
+
+    player_count = int(input().strip())
+
+    player = list(map(int, input().rstrip().split()))
+
+    result = climbingLeaderboard(ranked, player)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
